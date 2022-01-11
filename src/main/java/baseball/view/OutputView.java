7d8e2179ec3899cd863d591e9aed312ca3486159
jiwoo <sgo8308@naver.com>
2022-01-11 15:27:29 +0900

    feat(baseball.domain) : add println() to OutputView

--- src/main/java/baseball/view/OutputView.java
+++ src/main/java/baseball/view/OutputView.java
@@ -6,6 +6,10 @@ public class OutputView {
     }
 
     public static void print(String message) {
+        System.out.print(message);
+    }
+
+    public static void println(String message) {
         System.out.println(message);
     }
 }
