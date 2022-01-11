89c6dd47e1f1fa004cb2efff0c1dc92ad752284c
jiwoo <sgo8308@naver.com>
2022-01-11 15:03:13 +0900

    feat(baseball.view) : create OutputView and implement print()

---
+++ src/main/java/baseball/view/OutputView.java
@@ -0,0 +1,11 @@
+package baseball.view;
+
+public class OutputView {
+
+    private OutputView() {
+    }
+
+    public static void print(String message) {
+        System.out.println(message);
+    }
+}
